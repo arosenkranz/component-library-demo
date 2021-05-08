a593c41f63ad2a9fc30b86f4a245c9ead00089d7
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2021-05-08 11:06:54 -0400

    feat: creates starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GlobalStyle } from './utils';
-import { Button } from './components/Button';
 import { Logo } from './components/Logo';
 import { Header, Nav, NavItem } from './components/Nav';
 
@@ -17,23 +16,6 @@ function App() {
           <NavItem href='#'>Contact</NavItem>
         </Nav>
       </Header>
-      <div style={{ margin: '16px 0' }}>
-        <Button>This is a button</Button>
-      </div>
-      <div style={{ margin: '16px 0' }}>
-        <Button variant='secondary'>This is a secondary button</Button>
-      </div>
-      <div style={{ margin: '16px 0' }}>
-        <Button size='small'>This is a small button</Button>
-      </div>
-      <div style={{ margin: '16px 0' }}>
-        <Button size='large' variant='secondary'>
-          This is a large secondary button
-        </Button>
-      </div>
-      <div style={{ margin: '16px 0' }}>
-        <Button disabled>This is a disabled button</Button>
-      </div>
     </main>
   );
 }
